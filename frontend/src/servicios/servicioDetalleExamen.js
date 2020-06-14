8b935d1d3fb897b8f73e67da663dd8807244a882
rafucisneros <rafucisneros@gmail.com>
2020-06-14 18:30:33 -0400

    Secciones en detalles del examen

--- frontend/src/servicios/servicioDetalleExamen.js
+++ frontend/src/servicios/servicioDetalleExamen.js
@@ -12,3 +12,15 @@ export function getExamConfiguration(req, id){
       return Promise.reject(error);
   });
 }
+
+export function getExamSections(req, id){
+  let url = global.GET_EXAM_SECTIONS.replace("_id_", id);
+
+  return Axios.get(url, req)
+  .then((response) => {
+      return response;
+  })
+  .catch( (error) => {
+      return Promise.reject(error);
+  });
+}
--- frontend/src/vistas/TestDetailsPage.js
+++ frontend/src/vistas/TestDetailsPage.js
@@ -3,6 +3,11 @@ import React, {
     useState,
     Fragment
  } from 'react';
+import uuid from 'uuid/v1';
+
+import ListaSecciones from '../componentes/lista_secciones/ListaSecciones.js';
+import ListaEstudiantes from '../componentes/lista_estudiantes/ListaEstudiantes.js';
+import { useCreateTestPage } from '../context/createTestPageContext';
 
 // material
 import Container from '@material-ui/core/Container';
@@ -12,7 +17,7 @@ import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import { getExamConfiguration } from '../servicios/servicioDetalleExamen'
+import { getExamConfiguration, getExamSections } from '../servicios/servicioDetalleExamen'
 import { getExam, getExamQuestions } from '../servicios/servicioPresentarExamen'
 import Loading from '../componentes/loading/Loading.js';
 import { Divider } from '@material-ui/core';
@@ -27,12 +32,12 @@ export default function TestDetailsPage(props){
         setBasicConfigExpanded(!basicConfigExpanded);
     };
     
-    const [questionsExpanded, setQuestionsExpanded] = React.useState(true);
+    const [questionsExpanded, setQuestionsExpanded] = React.useState(false);
     const handleChangeQuestionsExpanded = () => {
         setQuestionsExpanded(!questionsExpanded);
     };
 
-    const [listSectionsExpanded, setListSectionsExpanded] = React.useState(true);
+    const [listSectionsExpanded, setListSectionsExpanded] = React.useState(false);
     const handleChangeListSectionsExpanded = () => {
         setListSectionsExpanded(!listSectionsExpanded);
     };
@@ -41,15 +46,57 @@ export default function TestDetailsPage(props){
         let fetchData = async () => {
             let { data: examConfig } = await getExamConfiguration({}, examID);
             let { data: examen } = await getExam({}, examID);
-            let { data: Preguntas } = await getExamQuestions({}, examID);
+            let {data} = await getExamQuestions({}, examID);
+            let { questions: Preguntas } = data;
+            let { data: Sections} = await getExamSections({}, examID);
+            
             let newExam = {
                 ...examen,
-                Preguntas
+                Preguntas,
+                Sections
             }
-            setExam(newExam)
+            setExam(newExam);
         }
         fetchData()
     }, [])
+
+    // Secciones Andres
+    const {  
+        handleChangeStep,
+        handleChangeComp,
+        secciones,
+        tipoConfiguracion
+    } = useCreateTestPage();
+
+    const handleAgregarEstudiante = (estudiante) => handleChangeComp(estudiante, 'estudiantes');
+
+    const handleSeleccionarSeccion = (id) => {
+        if (secciones.length > 0){
+            for( let seccion of secciones){
+                if(seccion.id === id) {
+                    handleChangeComp(seccion, 'seccion_seleccionada');
+                    break;
+                }    
+            }
+        }
+    }
+
+    const handleAgregarSecciones = () => {
+        let data = [...secciones];
+        data.push({
+            id: uuid(),
+            estudiantes : []
+        });
+        handleChangeComp(data, 'secciones');
+    }
+
+    const handleEliminarSecciones = (index) => {
+        let data = [...secciones];
+        data.splice(index, 1);
+        handleChangeComp(data, 'secciones');
+    }
+    // Fin Secciones Andres
+
     if(exam){
 
         return (
@@ -131,11 +178,11 @@ export default function TestDetailsPage(props){
                         {/* Preguntas */}
                         <ExpansionPanel expanded={questionsExpanded} onChange={handleChangeQuestionsExpanded}>
                             <ExpansionPanelSummary
-                            expandIcon={<ExpandMoreIcon />}
-                            aria-controls="panel1a-content"
-                            id="panel1a-header"
+                                expandIcon={<ExpandMoreIcon />}
+                                aria-controls="panel1a-content"
+                                id="panel1a-header"
                             >
-                            <Typography>Preguntas</Typography>
+                                <Typography>Preguntas</Typography>
                             </ExpansionPanelSummary>
                             <Divider />
                             <ExpansionPanelDetails>
@@ -217,21 +264,35 @@ export default function TestDetailsPage(props){
                             </ExpansionPanelDetails>
                         </ExpansionPanel>
                         {/* Lista Secciones */}
-                        <ExpansionPanel expanded={listSectionsExpanded} onChange={handleChangeListSectionsExpanded}>
+                        <ExpansionPanel expanded={listSectionsExpanded} onChange={handleChangeListSectionsExpanded} style={{width: "100%"}}>
                             <ExpansionPanelSummary
-                            expandIcon={<ExpandMoreIcon />}
-                            aria-controls="panel1a-content"
-                            id="panel1a-header"
+                                expandIcon={<ExpandMoreIcon />}
+                                aria-controls="panel1a-content"
+                                id="panel1a-header"
                             >
-                            <Typography>Lista de Secciones</Typography>
+                                <Typography>Secciones</Typography>
                             </ExpansionPanelSummary>
+                            <Divider />
                             <ExpansionPanelDetails>
-                            <Typography>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-                                sit amet blandit leo lobortis eget.
-                            </Typography>
+                                <Grid container>
+                                <Grid container>
+                                    <Grid item lg={4} md={4} xl={4} xs={12}>
+                                        <ListaSecciones 
+                                            handleAgregarSecciones = {handleAgregarSecciones}
+                                            handleEliminarSecciones = {handleEliminarSecciones}
+                                            handleSeleccionarSeccion = {handleSeleccionarSeccion}
+                                        />
+                                    </Grid>
+                                    <Grid item lg={8} md={8} xl={8} xs={12}>
+                                        <ListaEstudiantes 
+                                            handleAgregarEstudiante = {handleAgregarEstudiante}
+                                        />
+                                    </Grid>
+                                </Grid>
+                                </Grid>
                             </ExpansionPanelDetails>
                         </ExpansionPanel>
+                        {/*Estadisticas*/}
                     </Grid>
                 </Container>
             </div>
