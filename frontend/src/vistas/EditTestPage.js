a21a09f2fa25f21b96e6a0f2f09c13f06d25d343
Andrés Buelvas <ab@smarthop.co>
2020-06-15 20:09:46 -0400

    borrar tabla

--- frontend/src/vistas/EditTestPage.js
+++ frontend/src/vistas/EditTestPage.js
@@ -1,8 +1,5 @@
 import React, { Fragment } from 'react';
 
-// componentes
-import TableList from '../componentes/tables/TableList.js';
-
 // material
 import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
@@ -19,9 +16,6 @@ export default function EditTestPage(){
         <Container maxWidth={false} style={{paddingTop: '88px'}}>
             <Grid container spacing={3} style={{display: 'contents'}}>
                 <Grid item xs={12} md={12} lg={12}>
-                    <TableList>
-
-                    </TableList>
                 </Grid>
             </Grid>
         </Container>
